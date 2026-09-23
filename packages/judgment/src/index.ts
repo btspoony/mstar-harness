e78946028a2d15f8fecb10c682794b8e288b4ade
Tang Bohao <tech@btang.cn>
2026-09-24 07:14:34 +0800

    feat(judgment): add versioned native contracts

---
+++ packages/judgment/src/index.ts
@@ -0,0 +1,21 @@
+export {
+  CONTRACT_REVISION,
+  MAX_RUN_RESERVED_INPUT_TOKENS,
+  NATIVE_ENDPOINT,
+  NATIVE_MODEL,
+  PACK_SCHEMA,
+  PILOT_SCHEMA,
+  PROTOCOL_NUMERIC_TOLERANCE,
+  TOKEN_POLICY_METHOD,
+  TOKEN_RESERVATION_PER_ATTEMPT,
+  validatePack,
+  validatePilot,
+} from "./contracts.js";
+export type {
+  A05Label,
+  JudgmentPilot,
+  Profile,
+  ReviewDecisionPack,
+  ReviewTier,
+  WorkUnit,
+} from "./contracts.js";
